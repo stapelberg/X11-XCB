0c62573b16687927e4f3aa17846198388d2e9347
Michael Stapelberg <michael@stapelberg.de>
2009-07-15 01:04:23 +0200

    Initial commit

---
+++ lib/X11/XCB/Connection.pm
@@ -0,0 +1,23 @@
+package X11::XCB::Connection;
+# singleton, because you usually only need one connection to X
+# passes all methods to the underlying XCBConnection.
+
+use MooseX::Singleton;
+use Mytest qw(:all);
+
+has 'display' => (is => 'rw', isa => 'Str');
+has 'conn' => (is => 'rw', isa => 'XCBConnectionPtr', handles => qr/.*/);
+
+sub connect {
+	my $class = shift;
+	my $display = shift;
+
+	# TODO: do we need this one?
+	my $screens;
+	my $conn = Mytest->new($display, $screens);
+
+	X11::XCB::Connection->display($display);
+	X11::XCB::Connection->conn($conn);
+}
+
+1
