9e5d4833eec829f012486b8b68059976f9350e17
Michael Stapelberg <michael@stapelberg.de>
2009-07-16 15:03:02 +0200

    add modeline, retab!

--- lib/X11/XCB/Atom.pm
+++ lib/X11/XCB/Atom.pm
@@ -9,28 +9,29 @@ has '_sequence' => (is => 'rw', isa => 'Int');
 has '_conn' => (is => 'ro', default => sub { X11::XCB::Connection->instance });
 
 sub _build_id {
-	my $self = shift;
+    my $self = shift;
 
-	my $id = $self->_conn->intern_atom_reply($self->_sequence)->{atom};
+    my $id = $self->_conn->intern_atom_reply($self->_sequence)->{atom};
 
-	# None = 0 means the atom does not exist
-	die "No such atom" if ($id == 0);
+    # None = 0 means the atom does not exist
+    die "No such atom" if ($id == 0);
 
-	return $id;
+    return $id;
 }
 
 sub _request {
-	my $self = shift;
+    my $self = shift;
 
-	# Place the request directly after the name is set, we get the response later
-	my $request = $self->_conn->intern_atom(
-			1, # do not create the atom if it does not exist
-			length($self->name),
-			$self->name
-	);
+    # Place the request directly after the name is set, we get the reply later
+    my $request = $self->_conn->intern_atom(
+        1, # do not create the atom if it does not exist
+        length($self->name),
+        $self->name
+    );
 
-	# Save the sequence to identify the response
-	$self->_sequence($request->{sequence});
+    # Save the sequence to identify the response
+    $self->_sequence($request->{sequence});
 }
 
 1
+# vim:ts=4:sw=4:expandtab
