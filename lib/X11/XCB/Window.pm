308dd0fd3af41cbe259f44fea37afa8b3e070163
Michael Stapelberg <michael@stapelberg.de>
2009-07-16 13:40:19 +0200

    Add modeline, retab! and reformat

--- lib/X11/XCB/Window.pm
+++ lib/X11/XCB/Window.pm
@@ -19,9 +19,9 @@ has '_conn' => (is => 'ro', default => sub { X11::XCB::Connection->instance });
 has '_mapped' => (is => 'rw', isa => 'Int', default => 0);
 
 sub _build_id {
-	my $self = shift;
+    my $self = shift;
 
-	return $self->_conn->generate_id();
+    return $self->_conn->generate_id();
 }
 
 =head2 rect
@@ -34,159 +34,163 @@ window managers.
 
 =cut
 sub rect {
-	my $self = shift;
+    my $self = shift;
 
-	# Return the planned geometry if we’re not yet mapped
-	return $self->_rect unless $self->_mapped;
+    # Return the planned geometry if we’re not yet mapped
+    return $self->_rect unless $self->_mapped;
 
-	my $conn = $self->_conn;
+    my $conn = $self->_conn;
 
-	# Get the relative geometry
-	my $cookie = $conn->get_geometry($self->id);
-	my $relative_geometry = $conn->get_geometry_reply($cookie->{sequence});
+    # Get the relative geometry
+    my $cookie = $conn->get_geometry($self->id);
+    my $relative_geometry = $conn->get_geometry_reply($cookie->{sequence});
 
-	my $last_window_id = $self->id;
+    my $last_window_id = $self->id;
 
-	while (1) {
-		$cookie = $conn->query_tree($last_window_id);
-		my $reply = $conn->query_tree_reply($cookie->{sequence});
+    while (1) {
+        $cookie = $conn->query_tree($last_window_id);
+        my $reply = $conn->query_tree_reply($cookie->{sequence});
 
-		# If this is the root window, we stop here
-		last if ($reply->{root} == $reply->{parent});
+        # If this is the root window, we stop here
+        last if ($reply->{root} == $reply->{parent});
 
-		$last_window_id = $reply->{parent};
-	}
+        $last_window_id = $reply->{parent};
+    }
 
-	# If this window is a direct child of the root window, the relative
-	# geometry is equal to the absolute geometry
-	return $relative_geometry if ($last_window_id == $self->id);
+    # If this window is a direct child of the root window, the relative
+    # geometry is equal to the absolute geometry
+    return $relative_geometry if ($last_window_id == $self->id);
 
-	$cookie = $conn->get_geometry($last_window_id);
-	my $absolute_geometry = $conn->get_geometry_reply($cookie->{sequence});
+    $cookie = $conn->get_geometry($last_window_id);
+    my $absolute_geometry = $conn->get_geometry_reply($cookie->{sequence});
 
-	return X11::XCB::Rect->new(
-			x => $absolute_geometry->{x} + $relative_geometry->{x},
-			y => $absolute_geometry->{y} + $relative_geometry->{y},
-			width => $relative_geometry->{width},
-			height => $relative_geometry->{height},
-	);
+    return X11::XCB::Rect->new(
+            x => $absolute_geometry->{x} + $relative_geometry->{x},
+            y => $absolute_geometry->{y} + $relative_geometry->{y},
+            width => $relative_geometry->{width},
+            height => $relative_geometry->{height},
+    );
 }
 
 sub create {
-	my $self = shift;
-	my $root_window = $self->_conn->get_root_window();
-	my $mask = 0;
-	my @values;
+    my $self = shift;
+    my $root_window = $self->_conn->get_root_window();
+    my $mask = 0;
+    my @values;
 
-	if ($self->background_color) {
-		$mask |= CW_BACK_PIXEL;
-		push @values, $self->background_color;
-	}
+    if ($self->background_color) {
+        $mask |= CW_BACK_PIXEL;
+        push @values, $self->background_color;
+    }
 
 
-	if ($self->override_redirect == 1) {
-		$mask |= CW_OVERRIDE_REDIRECT;
-		push @values, 1;
-	}
+    if ($self->override_redirect == 1) {
+        $mask |= CW_OVERRIDE_REDIRECT;
+        push @values, 1;
+    }
 
-	$self->_conn->create_window(
-			WINDOW_CLASS_COPY_FROM_PARENT,
-			$self->id,
-			$self->_conn->get_root_window(),
-			$self->_rect->x,
-			$self->_rect->y,
-			$self->_rect->width,
-			$self->_rect->height,
-			0, # border
-			$self->class,
-			0, # copy visual TODO
-			$mask,
-			@values
-	);
+    $self->_conn->create_window(
+            WINDOW_CLASS_COPY_FROM_PARENT,
+            $self->id,
+            $self->_conn->get_root_window(),
+            $self->_rect->x,
+            $self->_rect->y,
+            $self->_rect->width,
+            $self->_rect->height,
+            0, # border
+            $self->class,
+            0, # copy visual TODO
+            $mask,
+            @values
+    );
 }
 
 sub map {
-	my $self = shift;
+    my $self = shift;
 
-	$self->_conn->map_window($self->id);
-	$self->_conn->flush;
-	$self->_mapped(1);
+    $self->_conn->map_window($self->id);
+    $self->_conn->flush;
+    $self->_mapped(1);
 }
 
 sub _update_name {
-	my $self = shift;
-	my $atomname = X11::XCB::Atom->new(name => '_NET_WM_NAME');
-	my $atomtype = X11::XCB::Atom->new(name => 'UTF8_STRING');
-	my $strlen;
+    my $self = shift;
+    my $atomname = X11::XCB::Atom->new(name => '_NET_WM_NAME');
+    my $atomtype = X11::XCB::Atom->new(name => 'UTF8_STRING');
+    my $strlen;
 
-	# Disable UTF8 mode to get the raw amount of bytes in this string
-	{ use bytes; $strlen = length($self->name); }
+    # Disable UTF8 mode to get the raw amount of bytes in this string
+    { use bytes; $strlen = length($self->name); }
 
-	$self->_conn->change_property(
-			PROP_MODE_REPLACE,
-			$self->id,
-			$atomname->id,
-			$atomtype->id,
-			8, 		# 8 bit per entity
-			$strlen, 	# length(name) entities
-			$self->name
-	);
-	$self->_conn->flush;
+    $self->_conn->change_property(
+            PROP_MODE_REPLACE,
+            $self->id,
+            $atomname->id,
+            $atomtype->id,
+            8,      # 8 bit per entity
+            $strlen,    # length(name) entities
+            $self->name
+    );
+    $self->_conn->flush;
 }
 
 sub _update_fullscreen {
-	my $self = shift;
-	my $conn = $self->_conn;
-	my $atomname = X11::XCB::Atom->new(name => '_NET_WM_STATE');
+    my $self = shift;
+    my $conn = $self->_conn;
+    my $atomname = X11::XCB::Atom->new(name => '_NET_WM_STATE');
 
-	# If we’re already mapped, we have to send a client message to the root window
-	# containing our request to change the _NET_WM_STATE atom.
-	#
-	# (See EWMH → Application window properties)
-	if ($self->_mapped) {
-		my %event = (
-				response_type => CLIENT_MESSAGE,
-				format => 32,	# 32-bit values
-				sequence => 0, 	# filled in by xcb
-				window => $self->id,
-				type => $atomname->id,
-			    );
+    # If we’re already mapped, we have to send a client message to the root
+    # window containing our request to change the _NET_WM_STATE atom.
+    #
+    # (See EWMH → Application window properties)
+    if ($self->_mapped) {
+        my %event = (
+                response_type => CLIENT_MESSAGE,
+                format => 32,   # 32-bit values
+                sequence => 0,  # filled in by xcb
+                window => $self->id,
+                type => $atomname->id,
+        );
 
-		my $packed = pack('CCSLL(LLLL)',
-				$event{response_type},
-				$event{format},
-				$event{sequence},
-				$event{window},
-				$event{type},
-				_NET_WM_STATE_TOGGLE,
-				X11::XCB::Atom->new(name => '_NET_WM_STATE_FULLSCREEN')->id,
-				0,
-				1, # normal application
-		);
+        my $packed = pack('CCSLL(LLLL)',
+                $event{response_type},
+                $event{format},
+                $event{sequence},
+                $event{window},
+                $event{type},
+                _NET_WM_STATE_TOGGLE,
+                X11::XCB::Atom->new(name => '_NET_WM_STATE_FULLSCREEN')->id,
+                0,
+                1, # normal application
+        );
 
-		$conn->send_event(0, $conn->get_root_window(), EVENT_MASK_SUBSTRUCTURE_REDIRECT, $packed);
-	} else {
-		my $atomtype = X11::XCB::Atom->new(name => 'ATOM');
-		my $atom;
-		if ($self->fullscreen) {
-			$atom = X11::XCB::Atom->new(name => '_NET_WM_STATE_FULLSCREEN');
-		} else {
-			$atom = X11::XCB::Atom->new(name => '_NET_WM_STATE_NORMAL');
-		}
+        $conn->send_event(
+                0, # don’t propagate (= send only to matching clients)
+                $conn->get_root_window(),
+                EVENT_MASK_SUBSTRUCTURE_REDIRECT,
+                $packed
+        );
+    } else {
+        my $atomtype = X11::XCB::Atom->new(name => 'ATOM');
+        my $atom = X11::XCB::Atom->new(
+                name => ($self->fullscreen ?
+                            '_NET_WM_STATE_NORMAL' :
+                            '_NET_WM_STATE_FULLSCREEN')
+        );
 
-		$conn->change_property(
-				PROP_MODE_REPLACE,
-				$self->id,
-				$atomname->id,
-				$atomtype->id,
-				32, 		# 32 bit integer
-				1,
-				pack('L', $atom->id)
-		);
-	}
+        $conn->change_property(
+                PROP_MODE_REPLACE,
+                $self->id,
+                $atomname->id,
+                $atomtype->id,
+                32,         # 32 bit integer
+                1,
+                pack('L', $atom->id)
+        );
+    }
 
-	$conn->flush;
+    $conn->flush;
 }
 
-
 1
+# vim:ts=4:sw=4:expandtab
