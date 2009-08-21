ccdb66fb5923c11ccbc3b0fd97d6935a92fdcd5a
Michael Stapelberg <michael@stapelberg.de>
2009-08-21 15:54:18 +0200

    Implement X11::XCB::Color, add ::Screen to git

---
+++ lib/X11/XCB/Color.pm
@@ -0,0 +1,30 @@
+package X11::XCB::Color;
+
+use Moose;
+use Data::Dumper;
+
+has 'hexcode' => (is => 'ro', isa => 'Str', required => 1);
+has 'pixel' => (is => 'ro', isa => 'Int', lazy_build => 1);
+has '_conn' => (is => 'ro', default => sub { X11::XCB::Connection->instance });
+
+=head2 pixel
+
+Returns the colorpixel (think of an ID) of this color. Works with TrueColor
+displays only at the moment.
+
+=cut
+sub _build_pixel {
+    my $self = shift;
+    my $hex = $self->hexcode;
+
+    # Strip optional leading # from hex code
+    $hex =~ s/^#//;
+
+    my @parts = unpack('CCC', pack('H2H2H2', unpack('(A2)*', $hex)));
+    my $color = ($parts[0] << 16) + ($parts[1] << 8) + $parts[2];
+
+    return $color
+}
+
+1
+# vim:ts=4:sw=4:expandtab
---
+++ lib/X11/XCB/Screen.pm
@@ -0,0 +1,20 @@
+package X11::XCB::Screen;
+
+use Moose;
+use X11::XCB::Rect;
+
+has 'rect' => (is => 'ro', isa => 'X11::XCB::Rect', required => 1);
+
+=head2 primary
+
+Returns true if this screen is the primary screen (that is, at position 0x0).
+
+=cut
+sub primary {
+    my $self = shift;
+
+    return ($self->rect->x == 0 && $self->rect->y == 0);
+}
+
+1
+# vim:ts=4:sw=4:expandtab
