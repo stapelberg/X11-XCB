d06efca681bfd3787ace7109dd27109c681d6663
Sergei Zhmylev <zhmylove@narod.ru>
2023-09-05 22:45:46 +0300

    Implement PropertyNotify and fix missing pad0
    X11 sometimes stores important data in pad0 field, so we cannot drop it.

--- lib/X11/XCB/Event/ConfigureRequest.pm
+++ lib/X11/XCB/Event/ConfigureRequest.pm
@@ -3,7 +3,7 @@ package X11::XCB::Event::ConfigureRequest;
 use Mouse;
 
 # XXX: the following are filled in by XS
-has [ 'response_type', 'sequence', 'parent', 'stack_mode', 'window', 'sibling', 'x', 'y', 'width', 'height', 'border_width', 'value_mask' ] => (is => 'ro', isa => 'Int');
+has [ 'response_type', 'sequence', 'pad0', 'parent', 'stack_mode', 'window', 'sibling', 'x', 'y', 'width', 'height', 'border_width', 'value_mask' ] => (is => 'ro', isa => 'Int');
 
 __PACKAGE__->meta->make_immutable;
 
--- lib/X11/XCB/Event/KeyRelease.pm
+++ lib/X11/XCB/Event/KeyRelease.pm
@@ -3,7 +3,7 @@ package X11::XCB::Event::KeyRelease;
 use Mouse;
 
 # XXX: the following are filled in by XS
-has [ 'detail', 'time', 'root', 'event', 'child', 'root_x', 'root_y', 'event_x', 'event_y', 'state', 'same_screen' ] => (is => 'ro', isa => 'Int');
+has [ 'response_type', 'sequence', 'pad0', 'detail', 'time', 'root', 'event', 'child', 'root_x', 'root_y', 'event_x', 'event_y', 'state', 'same_screen' ] => (is => 'ro', isa => 'Int');
 
 __PACKAGE__->meta->make_immutable;
 
